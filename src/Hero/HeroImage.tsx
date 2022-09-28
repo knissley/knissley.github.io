2f9f7d7b8367beaab2f7a0c9e7177e56b2bdf5a3
Kyle Nissley <knissley97@gmail.com>
2022-09-27 21:03:09 -0500

    Disable draggable photo on mobile

--- src/Hero/HeroImage.tsx
+++ src/Hero/HeroImage.tsx
@@ -2,15 +2,24 @@ import React, { FC } from 'react';
 import image from '../assets/HeroImageLarge.png';
 import './HeroImage.css';
 import { motion } from 'framer-motion';
+import { useMediaQuery } from 'react-responsive';
 
 interface Props {}
 
 const HeroImage:FC<Props> = () => {
-
+  const isLargerScreen = useMediaQuery({
+    query: '(min-width: 700px)',
+  });
 
   return (
     <div className='hero-image-container'>
-      <motion.img drag dragConstraints={{ top: -25, left: -25, right: 5, bottom: 25}} className='hero-image' alt='Kyle Nissley' src={image} />
+      {
+        isLargerScreen ?
+        <motion.img drag dragConstraints={{ top: -25, left: -25, right: 5, bottom: 25}} className='hero-image' alt='Kyle Nissley' src={image} />
+        :
+        <img className='hero-image' alt='Kyle Nissley' src={image}/>
+      }
+
     </div>
   )
 };
